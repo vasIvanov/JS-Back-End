565b4bc15fca854905a57c497f741614ca2ff35f
vasIvanov <viivanov.info@gmail.com>
2019-09-19 15:47:15 +0300

    display create cat and breed pages

---
+++ Cat-Shelter/handlers/cat.js
@@ -0,0 +1,87 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const qs = require('querystring');
+// const formidable = require('formidable');
+const cats = require('../data/cats');
+const breeds = require('../data/breeds');
+
+module.exports = (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+    
+    if(pathname === '/cats/add-cat' && req.method === 'GET') {
+
+        let filePath = path.normalize(
+            path.join(__dirname, '../views/addCat.html')
+        );
+
+        const index = fs.createReadStream(filePath);
+
+        index.on('data', (data) => {
+            res.write(data);
+        });
+
+        index.on('end', () => {
+            res.end();
+        });
+
+        index.on('error', (err) => {
+            console.log(err);
+            
+        })
+
+    } else if(pathname === '/cats/add-breed' && req.method === 'GET') {
+
+        let filePath = path.normalize(
+            path.join(__dirname, '../views/addBreed.html')
+        );
+
+        const index = fs.createReadStream(filePath);
+
+        index.on('data', (data) => {
+            res.write(data);
+        });
+
+        index.on('end', () => {
+            res.end();
+        });
+
+        index.on('error', (err) => {
+            console.log(err);
+            
+        })
+    } else if(pathname === '/cats/add-breed' && req.method === 'POST') {
+
+        let formData = '';
+        req.on('data', (data) => {
+            formData += data;
+            
+        });
+
+        req.on('end', () => {
+            let body = qs.parse(formData);
+
+            fs.readFile('./data/breeds.json', (err, data) => {
+                if(err) {
+                    throw err;
+                }
+
+                let breeds = JSON.parse(data);
+                breeds.push(body, breeds);
+                let json = JSON.stringify(breeds);
+                fs.writeFile('./data/breeds.json', json, 'utf-8', () => console.log('The breed was created succesfully'));
+
+            });
+
+            // res.redirect(301, {location: '/'});
+            res.end();
+            
+        });
+
+
+
+    } else if(pathname === '/cats/add-cat' && req.method === 'POST') {
+
+    }
+    
+}
