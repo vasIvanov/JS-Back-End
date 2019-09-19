5e316455bd66eac5693a305005d98e177ebcc331
vasIvanov <viivanov.info@gmail.com>
2019-09-19 12:40:32 +0300

    create file structure

---
+++ Cat-Shelter/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const port = 3000;
+
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Hello');
+}).listen(port);
+
+console.log(`Listening... ${port}`);
